b37dae69bc188a2aa4df433371b2b635593afb2d
Демидов Никита <yotumotopow@yandex.ru>
2022-10-27 15:08:55 +0300

    change ??

--- .github/workflows/scriptsForRelease/updateTicket.js
+++ .github/workflows/scriptsForRelease/updateTicket.js
@@ -2,6 +2,7 @@ import fetch from "node-fetch";
 
 async function updateTicket() {
   try {
+    console.log(process.env.COMMITS);
     const summary = `Релиз ${process.env.RELEASE} от ${new Date().toLocaleDateString()}`;
     const description = `Ответственный за релиз: ${process.env.ACTOR}
         ..............................
