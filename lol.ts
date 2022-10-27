6bee163036f928014386bb11255cfaae82b339ae
Демидов Никита <yotumotopow@yandex.ru>
2022-10-27 14:05:28 +0300

    change 1

--- lol.ts
+++ lol.ts
@@ -6,7 +6,7 @@ function heyHey(str: string): string {
 
 heyHey(str);
 
-const str1: string = "say my name!";
+const str1: string = "try this!";
 
 function heyHey1(str: string): string {
   return str.toUpperCase();
@@ -14,7 +14,7 @@ function heyHey1(str: string): string {
 
 heyHey1(str1);
 
-const str2: string = "say my name!";
+const str2: string = "ooops i did it again";
 
 function heyHey2(str: string): string {
   return str.toUpperCase();
